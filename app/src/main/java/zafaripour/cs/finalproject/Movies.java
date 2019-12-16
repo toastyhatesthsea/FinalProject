6198770d9b387a9b9d4fba05b83aeb07a4e8403b
Zafaripour <toastyhatesthesea@gmail.com>
2019-12-16 04:01:04 -0800

    Finished Movies activity

--- app/src/main/java/zafaripour/cs/finalproject/Movies.java
+++ app/src/main/java/zafaripour/cs/finalproject/Movies.java
@@ -2,10 +2,16 @@ package zafaripour.cs.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
@@ -19,18 +25,27 @@ public class Movies extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
     private final static String URL_STRING = "https://ghibliapi.herokuapp.com/films";
 
+    Button backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
-
-
-    }
-
-    public void goButtonClick(View v) {
         DownloadData downloadData = new DownloadData(this);
         downloadData.execute(URL_STRING);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainIntent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 
     private static class DownloadData extends AsyncTask<String, Void, String> {
@@ -52,8 +67,20 @@ public class Movies extends AppCompatActivity {
                 InputStream is = urlConnection.getInputStream();
                 Scanner aScanner = new Scanner(is);
                 aScanner.useDelimiter("\\A");
+
+                boolean hasInput = aScanner.hasNext();
+                if (hasInput) {
+                    jsonData = aScanner.next();
+                    System.out.println(jsonData);
+                } else {
+                    return activityWeakRef.get().getResources().
+                            getString(R.string.download_error);
+
+                }
+
             } catch (Exception excep) {
                 Log.d(TAG, excep.toString());
+
             }
             finally {
                 if (urlConnection != null) {
@@ -61,8 +88,45 @@ public class Movies extends AppCompatActivity {
                 }
             }
 
+            StringBuilder results = new StringBuilder();
+            String title;
+            String director;
+            String row;
 
-            return null;
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(jsonData);
+                System.out.println(jsonArray.getJSONObject(0));
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject book = jsonArray.getJSONObject(i);
+                    title = book.getString("title");
+                    director = book.getString("director");
+                    row = title + "\nDirected by: " + director + "\n\n";
+
+                    results.append(row);
+                }
+                return results.toString();
+
+            } catch (Exception ex) {
+                System.out.println("uhhhhhh");
+
+                Log.d(TAG, ex.toString());
+                return activityWeakRef.get().getResources().
+                        getString(R.string.data_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Movies aActivity = activityWeakRef.get();
+            if (aActivity == null || aActivity.isFinishing()) {
+                return;
+            }
+            TextView aTextView = aActivity.findViewById(R.id.output);
+            aTextView.setText(s);
         }
     }
 
