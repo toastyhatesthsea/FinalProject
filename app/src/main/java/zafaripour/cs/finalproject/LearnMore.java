03b4e44e5ec9427b2e276369b5e9a6a38429e2ed
Zafaripour <toastyhatesthesea@gmail.com>
2019-12-15 21:35:43 -0800

    Created Learn More activity

---
+++ app/src/main/java/zafaripour/cs/finalproject/LearnMore.java
@@ -0,0 +1,14 @@
+package zafaripour.cs.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnMore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_more);
+    }
+}
--- app/src/main/java/zafaripour/cs/finalproject/MainActivity.java
+++ app/src/main/java/zafaripour/cs/finalproject/MainActivity.java
@@ -2,13 +2,31 @@ package zafaripour.cs.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button learnMoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        learnMoreButton = findViewById(R.id.LearnMore);
+    }
+
+
+    private class LearnMoreListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            Intent learnMoreScreen = new Intent(getApplicationContext(), )
+
+        }
     }
 }
+
